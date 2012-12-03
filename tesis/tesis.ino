cc18e475a0ecffa8fb460ec24ceef596412646e2
Juan Carlos Rojas <jcarlos.rojas@crowdint.com>
2012-12-02 23:16:57 -0600

    Scheduler implemented
    NEW:
    -No more delays
    
    TODO:
    - Evaluate readings

--- tesis/tesis.ino
+++ tesis/tesis.ino
@@ -1,10 +1,34 @@
-int temp_sensor_pin = 5;
-int foto_sensor_pin = 4;
-int prox_sensor_pin = 3;
+const int temp_sensor_pin = 5;
+const int foto_sensor_pin = 4;
+const int prox_sensor_pin = 3;
 
 float temp;
 int foto;
 int prox;
+boolean flagT = false;
+boolean flagF = false;
+boolean flagP = false;
+
+int interval = 1000;
+unsigned long previousMillis;
+
+void imprimeVariables(){
+  Serial.print("Temp: ");  
+  Serial.print(temp);
+  Serial.print("\nFoto: ");
+  Serial.println(foto);
+}
+
+void verificaTemp(){
+    if(temp > 26 && flagT==false){
+    flagT = true;
+    //enciendeAire();
+  }
+  if(temp < 20){
+    flagT = false;
+    //apagaAire();
+  }
+}
 
 void setup() {
   Serial.begin(9600); 
@@ -12,17 +36,21 @@ void setup() {
 }
 
 void loop() {
+  //Acquire readings
   temp = analogRead(temp_sensor_pin);
   temp = (5.0 * temp * 100.0)/1024;
-  
   foto = analogRead(foto_sensor_pin);
+  prox = analogRead(prox_sensor_pin);
   
-  Serial.print("Temp: ");  
-  Serial.println(temp);
-  
-  Serial.print("Foto: ");
-  Serial.println(foto);
+  //Evaluate readings
   
   
-  delay(1000);
+  //Print values every second
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > interval){
+    previousMillis = currentMillis;
+    
+    imprimeVariables();
+  }
+  
 }
